03dfc38e409c55464d87498385a1248b79cba0ef
Stephen Cowley <stephencowley44@gmail.com>
2023-12-01 20:09:28 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Installation of PROJECT."""
+
+
+from setuptools import setup
+
+setup(
+    name='PROJECT',
+    version='0.1.0',
+    packages=['PROJECT'],
+    package_data={
+        'PROJECT': ['py.typed']
+    },
+)
